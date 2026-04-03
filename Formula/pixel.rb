9404fc32cb4be0325d2e714b894409c17655c311
simon gauld <simon.gauld@gmail.com>
2026-04-03 20:08:39 +0100

    pixel 0.1.59

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -1,26 +1,26 @@
 class Pixel < Formula
   desc "Pixel - a command-line tool"
   homepage "https://github.com/simonski/pixel"
-  version "0.1.12"
+  version "0.1.59"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/pixel/releases/download/v0.1.12/pixel-0.1.12-darwin-arm64.tar.gz"
-      sha256 "941d069d8446255c390cdfdc70a9dd46cc33329d9a119beba1fe74eed8dfbd25"
+      url "https://github.com/simonski/pixel/releases/download/v0.1.59/pixel-0.1.59-darwin-arm64.tar.gz"
+      sha256 "24a9272fd902ade877abc18671307b252586f5838fe87f54e530d86411d729c4"
     else
-      url "https://github.com/simonski/pixel/releases/download/v0.1.12/pixel-0.1.12-darwin-amd64.tar.gz"
-      sha256 "4c9c31ba4665f3aff2e3821d85be7d701a1d9f9e64af7c9cc649e62f9ee9949c"
+      url "https://github.com/simonski/pixel/releases/download/v0.1.59/pixel-0.1.59-darwin-amd64.tar.gz"
+      sha256 "4d7667eb0d279aee283808e71e6f8dc52944a0782abeb019223fcc6017392a93"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/pixel/releases/download/v0.1.12/pixel-0.1.12-linux-arm64.tar.gz"
-      sha256 "98c33762e9c9ef39a5e3f591cc60c2dfc23d4ed3d064c63607c5e08470041366"
+      url "https://github.com/simonski/pixel/releases/download/v0.1.59/pixel-0.1.59-linux-arm64.tar.gz"
+      sha256 "b21a6dfe62151b58c59f9aad557e4f59f271f9b5ba4afca7d505bfcfb0e666f3"
     else
-      url "https://github.com/simonski/pixel/releases/download/v0.1.12/pixel-0.1.12-linux-amd64.tar.gz"
-      sha256 "3e73d60b923534be2a843c691752375ce97a1fd8482995f3ddeba6e171cdc6e4"
+      url "https://github.com/simonski/pixel/releases/download/v0.1.59/pixel-0.1.59-linux-amd64.tar.gz"
+      sha256 "5810dabd245a15744ceb4454f3e133019396c2b4180c008428845caac27adcb6"
     end
   end
 
