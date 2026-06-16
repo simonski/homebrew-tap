67e2089695b17d5066ac4406e6b1e8540b8a72f9
pixel release <pixel@users.noreply.github.com>
2026-06-16 23:08:07 +0100

    pixel 0.1.212

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -1,26 +1,26 @@
 class Pixel < Formula
   desc "Pixel - simplified container hosting for your homelab"
   homepage "https://github.com/simonski/pixel"
-  version "0.1.211"
+  version "0.1.212"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/pixel/releases/download/v0.1.211/pixel-0.1.211-darwin-arm64.tar.gz"
-      sha256 "bda672f3e965975a97ac0480fcbc78bb123740c4788aefdc45447548b8ccc25b"
+      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-darwin-arm64.tar.gz"
+      sha256 "74585fecfb70ea1ff8f4f96eced0a4e47e5773b0c4b24b7814204e76ad3af921"
     else
-      url "https://github.com/simonski/pixel/releases/download/v0.1.211/pixel-0.1.211-darwin-amd64.tar.gz"
-      sha256 "cda1fe07728cf5742f551ac16517f28f3dc96d9869a339206f408b30dced04df"
+      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-darwin-amd64.tar.gz"
+      sha256 "48376216b54ef91b73b0d5bd642b9584680354aec97e25fbf703a8bfac926e20"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/pixel/releases/download/v0.1.211/pixel-0.1.211-linux-arm64.tar.gz"
-      sha256 "cc9fc0f644493a77f39f2777e5b20834c199d1a07c76dd78a73eee61e7421a8a"
+      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-linux-arm64.tar.gz"
+      sha256 "d214015dae5df1438f3865e94e27b1e451c68e4e53cc2cb74fe6cc244c267f44"
     else
-      url "https://github.com/simonski/pixel/releases/download/v0.1.211/pixel-0.1.211-linux-amd64.tar.gz"
-      sha256 "921fd6cd5288ebf6e9c10fbceeff2ef05322fba45321c20961042265f4bfccd9"
+      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-linux-amd64.tar.gz"
+      sha256 "6ef0d4ecc93a667f26bc9ca1880f09500bb978e7b2801c3a2e1a5ab70058a45e"
     end
   end
 
