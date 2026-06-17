f36483fef7978481cbb36af96f18af1dc105c2ac
pixel release <pixel@users.noreply.github.com>
2026-06-17 19:41:34 +0100

    pixel 0.1.215

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -1,26 +1,26 @@
 class Pixel < Formula
   desc "Pixel - simplified container hosting for your homelab"
   homepage "https://github.com/simonski/pixel"
-  version "0.1.213"
+  version "0.1.215"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-darwin-arm64.tar.gz"
-      sha256 "dc194148698db6e63beeb2973369150b693de2515672e5fd726a9d3ddfdcc6c3"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.215/pixel-0.1.215-darwin-arm64.tar.gz"
+      sha256 "89e6347c8951b7c5a5f2b4270ac03c1fc3bab2d963e82ecd4980e7a8cb100fb7"
     else
-      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-darwin-amd64.tar.gz"
-      sha256 "98b71a21342d3fc062a46cba66244778533d98c555d539403d796c51dd587530"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.215/pixel-0.1.215-darwin-amd64.tar.gz"
+      sha256 "f6fa4b4d8322c31895593392c381be9371c020c4c9a14cfb49b9ba5a4cb418eb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-linux-arm64.tar.gz"
-      sha256 "d2abc950154893377096061e893b47a313aefa15c216ab26b5bd2b2380b37d8f"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.215/pixel-0.1.215-linux-arm64.tar.gz"
+      sha256 "bb50a02f69fe36f2bd6d6ccc765ce402a637140bd2bc92febca803e61ba85bb7"
     else
-      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-linux-amd64.tar.gz"
-      sha256 "2deb315dddf70174f46173385f2d3436d97762ebf01042cde8ce56782f5339ab"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.215/pixel-0.1.215-linux-amd64.tar.gz"
+      sha256 "cc7466cd01bee4bb86dbcec276d2b970dae4bf52d4e457aedc68255122afd823"
     end
   end
 
