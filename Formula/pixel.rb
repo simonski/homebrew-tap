9d702622a23dac3d51296ec920fb2b76abe5fab0
pixel release <pixel@users.noreply.github.com>
2026-06-17 19:37:09 +0100

    pixel 0.1.213

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -1,26 +1,26 @@
 class Pixel < Formula
   desc "Pixel - simplified container hosting for your homelab"
   homepage "https://github.com/simonski/pixel"
-  version "0.1.212"
+  version "0.1.213"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-darwin-arm64.tar.gz"
-      sha256 "74585fecfb70ea1ff8f4f96eced0a4e47e5773b0c4b24b7814204e76ad3af921"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-darwin-arm64.tar.gz"
+      sha256 "dc194148698db6e63beeb2973369150b693de2515672e5fd726a9d3ddfdcc6c3"
     else
-      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-darwin-amd64.tar.gz"
-      sha256 "48376216b54ef91b73b0d5bd642b9584680354aec97e25fbf703a8bfac926e20"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-darwin-amd64.tar.gz"
+      sha256 "98b71a21342d3fc062a46cba66244778533d98c555d539403d796c51dd587530"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-linux-arm64.tar.gz"
-      sha256 "d214015dae5df1438f3865e94e27b1e451c68e4e53cc2cb74fe6cc244c267f44"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-linux-arm64.tar.gz"
+      sha256 "d2abc950154893377096061e893b47a313aefa15c216ab26b5bd2b2380b37d8f"
     else
-      url "https://github.com/simonski/homebrew-tap/releases/download/v0.1.212/pixel-0.1.212-linux-amd64.tar.gz"
-      sha256 "6ef0d4ecc93a667f26bc9ca1880f09500bb978e7b2801c3a2e1a5ab70058a45e"
+      url "https://github.com/simonski/homebrew-tap/releases/download/pixel-v0.1.213/pixel-0.1.213-linux-amd64.tar.gz"
+      sha256 "2deb315dddf70174f46173385f2d3436d97762ebf01042cde8ce56782f5339ab"
     end
   end
 
